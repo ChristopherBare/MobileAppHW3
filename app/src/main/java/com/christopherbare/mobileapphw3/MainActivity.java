e282ac8d41e3c14c00ece58f00bae2ae8aead045
Christopher Bare <christopherbare@outlook.com>
2018-07-31 03:01:30 -0400

    Init commit. Login and signup functionality is working and connected to the database.

---
+++ app/src/main/java/com/christopherbare/mobileapphw3/MainActivity.java
@@ -0,0 +1,93 @@
+package com.christopherbare.mobileapphw3;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private final String TAG = "Demo";
+    EditText emailLogin, passwordLogin;
+    Button loginButton;
+    TextView text, signUpLink;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        emailLogin = (EditText) findViewById(R.id.emailLogin);
+        passwordLogin = (EditText) findViewById(R.id.passwordLogin);
+        loginButton = (Button) findViewById(R.id.buttonLogin);
+        text = (TextView) findViewById(R.id.textViewNotRegistered);
+        signUpLink = (TextView) findViewById(R.id.linkSignUp);
+
+        signUpLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignUp.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Login Button.", Toast.LENGTH_SHORT).show();
+                String email = emailLogin.getText().toString().trim();
+                String password = passwordLogin.getText().toString().trim();
+
+                if(email.equals("")){
+                    Toast.makeText(MainActivity.this, "Enter Email", Toast.LENGTH_SHORT).show();
+                }else if(password.equals("")){
+                    Toast.makeText(MainActivity.this, "Enter Password", Toast.LENGTH_SHORT).show();
+                }else{
+                    mAuth.signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(task.isSuccessful()){
+                                        Log.d("demo","signInWithEmail:success");
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        user.getIdToken(true);
+
+                                        Intent intent = new Intent(MainActivity.this, ChatActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }else{
+                                        Log.d("demo", "signInWithEmail:failure",task.getException());
+                                        Toast.makeText(MainActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+            }
+        });
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(mAuth.getCurrentUser() != null){
+            Intent i = new Intent(MainActivity.this, ChatActivity.class);
+            startActivity(i);
+            finish();
+        }
+    }
+}
