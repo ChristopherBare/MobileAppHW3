2bcc6468d224fe8a45d6e99b2634ca8a66fc3cae
Christopher Bare <christopherbare@outlook.com>
2018-07-31 03:38:08 -0400

    Implemented logout menu functionality.

--- app/src/main/java/com/christopherbare/mobileapphw3/ChatActivity.java
+++ app/src/main/java/com/christopherbare/mobileapphw3/ChatActivity.java
@@ -1,13 +1,83 @@
 package com.christopherbare.mobileapphw3;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ChatActivity extends AppCompatActivity {
+    TextView displayName;
+    DialogInterface.OnClickListener dialogClickListener;
+    private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+//        displayName = findViewById(R.id.displayName);
+//        displayName.setText(mAuth.getCurrentUser().getDisplayName());
+
+        dialogClickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which){
+                    case DialogInterface.BUTTON_POSITIVE:
+                        SharedPreferences prefs = getSharedPreferences("info", MODE_PRIVATE);
+                        SharedPreferences.Editor editor = prefs.edit();
+                        editor.putString("email", mAuth.getCurrentUser().getEmail());
+                        editor.commit();
+
+                        mAuth.signOut();
+                        Intent intent = new Intent(ChatActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                        break;
+
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        //No was clicked
+                        break;
+                }
+            }
+        };
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.app_bar, menu);
+        return true;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.logoutButton){
+            AlertDialog.Builder builder = new AlertDialog.Builder(ChatActivity.this);
+            builder .setMessage("Are you sure?")
+                    .setPositiveButton("Yes", dialogClickListener)
+                    .setNegativeButton("No", dialogClickListener).show();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
